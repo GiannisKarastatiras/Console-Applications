ba1ee9eacb7b3395f65c30293adab501d328381d
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:34:11 +0200

    Add files via upload

---
+++ InterfacesAdvancedDemo/InterfacesAdvancedDemo/Document.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAdvancedDemo
+{
+    public class Document : Printable, Storable 
+    {
+        public string DocName { get; set; }
+
+        public int Status
+        {
+            get
+            {
+                return 0;   
+            }
+        }
+
+        public void CountCharacters()
+        {
+            Console.WriteLine("133 Characters");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Writing Document to printer");
+        }
+
+        public string Read()
+        {
+            Console.WriteLine("Getting Document from File...");
+            return "Success";
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Writing Document to file...");
+        }
+    }
+}
---
+++ InterfacesAdvancedDemo/InterfacesAdvancedDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAdvancedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDoc = new Document();
+            myDoc.Print();
+            myDoc.CountCharacters();
+
+            //Printable mySecondDoc = new Printable();
+            Printable printableItem = myDoc;   
+            printableItem.Print();
+            //printableItem.CountCharacters();
+
+            Document theDoc = printableItem as Document;
+            if (theDoc != null)
+            {
+                theDoc.CountCharacters();
+            }
+
+            string contents = theDoc.Read();
+            Console.WriteLine("contents: " + contents);
+        }
+    }
+}
