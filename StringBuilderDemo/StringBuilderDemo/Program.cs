d9e1273b93eb37dfbdf66154a5e02fc4bbb0b6ba
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:18:20 +0200

    Add files via upload

---
+++ StringBuilderDemo/StringBuilderDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+
+
+            //builder.Append('-', 10);
+            //builder.AppendLine();
+            //builder.Append("Header");
+            //builder.AppendLine();
+            //builder.Append('-', 10);
+
+            //builder.Replace('-', '+');
+
+            //builder.Remove(0, 10);
+
+            //builder.Insert(0, new string('-', 10));
+
+
+            
+            builder
+                    .Append('-', 10)
+                    .AppendLine()
+                    .Append("Header")
+                    .AppendLine()
+                    .Append('-', 10);
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+
+
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Character: " + builder[0]);
+        }
+    }
+}
