61dc3325eeb4f1ef04bff530577839049bb85bd2
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:14:07 +0200

    Add files via upload

---
+++ StackDemo/StackDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //Stack ---LIFO---
+
+            var mystack = new Stack<int>();
+            mystack.Push(134);          //Push---like Enqueue
+            mystack.Push(112);
+
+            int val = mystack.Pop();    //Pop---like Dequeue
+            Console.WriteLine("val: {0}", val);
+
+            for (int i = 0; i < 50; i++)
+            {
+                mystack.Push(i);
+            }
+
+            foreach (int i in mystack)
+            {
+                Console.WriteLine("{0}", i);
+            }
+
+            var whichValue = mystack.Pop();
+            Console.WriteLine("\nPopped Value : {0}", whichValue);
+            whichValue = mystack.Pop();
+            Console.WriteLine("\nPopped Value : {0}", whichValue);                        
+        }
+    }
+}
