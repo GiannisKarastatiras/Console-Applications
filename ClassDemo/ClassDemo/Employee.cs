c320d617dbacd46c171959c8950ccb5e8763bc7c
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:08:39 +0200

    Add files via upload

---
+++ ClassDemo/ClassDemo/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Employee
+    {
+        //Properties
+        public int Age { get; set; }                 
+        //private int age;                            
+
+        //public int Age     //age field
+        //{
+        //    get { return age; }
+        //    set { age = value; }
+        //}
+
+        private string name;  //backend variable                  
+        public string Name
+        {       
+            get { return name; }  //name,age etc --- fields
+            set { name = value; }
+        }
+
+
+
+        //access modifier---public,private
+        //public int MyAge()          //Get method
+        //{
+        //    return age;
+        //}
+
+
+
+        //public void SetAge(int newAge)         //Set method     
+        //{                                      
+        //    age = newAge;            
+        //}
+
+        
+    }
+}
---
+++ ClassDemo/ClassDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Peri = new Employee();         //Objects or Instances
+            Peri.Age = 21;                                 
+            Console.WriteLine("Peri's age is {0}", Peri.Age);
+        }
+    }
+}
