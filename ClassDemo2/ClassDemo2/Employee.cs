e1b78a956a8b34ba34fac1562ff15e5b2c7555d7
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:09:33 +0200

    Add files via upload

---
+++ ClassDemo2/ClassDemo2/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo2
+{
+    class Employee
+    {
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }    
+        public double Salary { get; set; }
+        public DateTime StartingDate { get; set; }
+        public string PhoneNumber { get; set; }
+
+        //Default Contsructor
+        public Employee()
+        {
+        }
+
+        //Special method--Constructor
+        public Employee(int age, string name, double salary, DateTime startingDate, string phoneNumber)
+        {
+            Age = age;
+            Name = name;
+            Salary = salary;
+            StartingDate = startingDate;
+            PhoneNumber = phoneNumber;
+        }
+        
+
+        public double Bonus(double a)
+        {
+            Salary = Salary + (Salary * (a / 100));
+            return Salary;
+        }
+
+        
+        //public void SalaryWithBonus(double bonusPercent)
+        //{
+        //   Salary += Salary * bonusPercent;
+        //}
+        
+
+        
+   
+    }
+}
---
+++ ClassDemo2/ClassDemo2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee Peri = new Employee();
+            Employee Kostas = new Employee()            //initialization object                   
+            {
+                Age = 54,
+                Name = "Kostas Papadopoulos",
+                Salary = 654.888,
+                PhoneNumber = "210-2322334",
+                StartingDate = new DateTime(2015, 5, 22)            
+                
+            };
+
+
+
+            Console.WriteLine("Give me the number of bonus for your employees: ");           
+            double pososto = double.Parse(Console.ReadLine());
+
+            //Assigment
+            Peri.Age = 21;
+            Peri.Name = "Peri";
+            Peri.Salary = 654.87;          
+            Peri.PhoneNumber = "210-5665987";
+            Peri.StartingDate = new DateTime(2016, 06, 13);
+                       
+
+            Peri.Bonus(pososto);
+            Kostas.Bonus(pososto);
+
+            Console.WriteLine($"{Kostas.Name} is {Kostas.Age}, he started working on {Kostas.StartingDate} and makes {Kostas.Bonus(pososto)}");
+            
+            //Kostas.SalaryWithBonus(0.05);
+            Console.WriteLine($"{Peri.Name} is {Peri.Age}, he started working on {Peri.StartingDate} and makes {Peri.Bonus(pososto)}");
+        }
+        // Constructor
+        Employee Alex = new Employee(32, "Alex Alex", 456.58, new DateTime(2015, 2, 22), "2107884952");          //new = constructor
+
+        Employee Panos = new Employee();                 //Constructor overloaded
+        
+        
+    }
+}
