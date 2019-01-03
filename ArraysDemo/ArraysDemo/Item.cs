f7c0b169651dc86c17f6d4c757c0b3b1cbaf982e
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:11:34 +0200

    Add files via upload

---
+++ ArraysDemo/ArraysDemo/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace ArraysDemo
+{
+    class Item
+    {
+        public int ID { get; set; }
+
+        //Constructor
+        public Item(int id)
+        {
+            ID = id;
+        }
+    }
+}
---
+++ ArraysDemo/ArraysDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                          
+            Item[] items = new Item[5];       
+            Random r = new Random();               
+
+            for (int i = 0; i < items.Length; i++)    
+            {
+                items[i] = new Item(r.Next());
+            }
+
+            Console.WriteLine("Items: ");
+            foreach (Item item in items)
+            {
+                Console.WriteLine("ItemID = {0}", item.ID);     
+            }
+
+            
+            Item myItem = items[1];         //1---index operator
+            Console.WriteLine("My Item has ID = {0}", myItem.ID);
+
+            var Presidents = new String[5];                     
+            Presidents[0] = "Melissanidis";
+            Presidents[1] = "Marinakis";
+            Presidents[2] = "Alafouzos";
+
+            Console.WriteLine("Presidents: ");
+            foreach (string president in Presidents)
+            {
+                Console.WriteLine(president);
+            }
+
+            Array.Sort(Presidents);     
+
+
+            Console.WriteLine("Presidents: ");
+            foreach (string president in Presidents)
+            {
+                Console.WriteLine(president);
+            }
+        }           
+    }
+}
