ba99016a168bb6fe351f8f65de4cd15c21944067
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:15:52 +0200

    Add files via upload

---
+++ ExceptionsDemo/ExceptionsDemo/Tester.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsDemo
+{
+    class Tester
+    {
+        //----Procedural programming----//
+
+        public void Method1()
+        {
+            Console.WriteLine("Method 1 Begins...");
+            Method2();
+            Console.WriteLine("Method 1 Ends...");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method 2 Begins...");
+            Method3();
+            Console.WriteLine("Method 2 Ends...");
+        }
+        public void Method3()
+        {
+            Console.WriteLine("Method 3 Begins...");
+            Console.WriteLine("File Opened...");
+            
+            try
+            {
+                var x = 0;
+                var y = 13 / x;
+            }
+            catch (Exception e)    
+            {
+                Console.WriteLine("Caught Exception: {0}", e.Message);                
+            }
+            finally        
+            {
+                Console.WriteLine("File Closed...");
+            }
+            
+            Console.WriteLine("Method 3 Ends...");
+        }
+    }
+}
