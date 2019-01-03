c751723d9755a50e22891169a5138853d84da53f
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:12:23 +0200

    Add files via upload

---
+++ ListDemo/ListDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+
+            //Default Constructor List
+            List<int> myList = new List<int>();           
+            for (int i = 0; i < 10; i++)                   
+            {
+                myList.Add(r.Next());                    
+                                                         
+            }
+
+            myList.Sort();
+
+            foreach (int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
