790ee531a707255a7e2f500fcbd3ff872b2b9320
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:05:03 +0200

    Add files via upload

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string ageAsString = Console.ReadLine();                 
+            int age = int.Parse(ageAsString);
+
+            //conditional.
+            //Check for Voting.
+            if (age > 70)
+            {
+                Console.WriteLine("You are too old to vote.");
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You are an Adult.");             
+            }
+            else if (age < 18 )
+            {
+                Console.WriteLine("You cannot vote, too young.");
+            
+            }
+
+            else
+            {
+                Console.WriteLine("Something getting wrong");
+            }
+            // End Check for Voting.
+        }
+    }
+}
---
+++ Conditionals/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your age?");
+            string ageAsString = Console.ReadLine();
+            int age = int.Parse(ageAsString);
+
+            // Check for Voting
+            if (age > 70)
+            {
+                Console.WriteLine("You are too old to vote.");
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You are an Adult.");
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("You cannot vote, too young.");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            // End Check for Voting
+
+        }
+    }
+}
