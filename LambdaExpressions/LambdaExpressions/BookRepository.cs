208e63d11e9e35f187636ff7bc46c9ad207a8d1a
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:36:55 +0200

    Add files via upload

---
+++ LambdaExpressions/LambdaExpressions/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>()
+            {
+                new Book() { Title = "Book One", Price = 5},
+                new Book() { Title = "Book Two", Price = 12},
+                new Book() { Title = "Book Three", Price = 19}
+            };
+        }
+    }
+}
---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions //use for convinience 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arfs => expression       
+            // x => ...
+            //(x, y, z) => ...
+            //delegate Func<>
+            Func<int, int> square = number => number * number;   //number => number * number---lambda expression                        
+            Console.WriteLine(square(5));
+
+            const int factor = 6;
+            Func<int, int> multiplier = n => n * factor;
+            var result = multiplier(10);
+            Console.WriteLine(result);
+
+            //Book Example
+            var books = new BookRepository().GetBooks(); 
+            //Predicate<T>delegate match means that returns a boolean
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+
+        //static int Square(int number)
+        //{
+        //    return number * number;
+        //}
+
+        //static bool IsCheaperThan10Euros(Book book)
+        //{
+        //    return book.Price < 10;
+        //}
+    }
+}
