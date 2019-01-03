f54a4c863be2dc07d3c6c610f589e7f1ac6ed644
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:14:45 +0200

    Add files via upload

---
+++ DictionaryDemo/DictionaryDemo/Nomos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    class Nomos
+    {
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public int Size { get; set; }
+
+        public Nomos(string capital, int population, int size)
+        {
+            Capital = capital;
+            Population = population;
+            Size = size;
+        }
+
+        public static Dictionary<string, Nomos> GetNomoi()
+        {
+            var nomoi = new Dictionary<string, Nomos>();
+
+            var nomos = new Nomos("Athina", 576, 334);
+            nomoi.Add("Attiki", nomos);
+            nomos = new Nomos("Katerini", 80, 21);
+            nomoi.Add("Pieria", nomos);
+            nomos = new Nomos("Volos", 56, 8);
+            nomoi.Add("Magnisia", nomos);
+            
+            return nomoi;
+        }
+        
+    } 
+        
+}
---
+++ DictionaryDemo/DictionaryDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constructor class Dictionary                                    //string string ---pair of key value
+            Dictionary<string, string> capitals = new Dictionary<string, string>();     
+            capitals.Add("Attiki", "Athina");
+            capitals.Add("Pieria", "Katerini");
+            capitals.Add("Magnisia", "Volos");
+            capitals.Add("Imathias", "Veroia");
+
+            string capitalOfAttiki = capitals["Attiki"];    //---[magic string]
+            Console.WriteLine("The capital of Attiki is {0}", capitalOfAttiki);
+
+            var nomoi = Nomos.GetNomoi();
+            var attiki = nomoi["Attiki"];
+            Console.WriteLine("The capital of Attiki is {0}, population is {1}", attiki.Capital, attiki.Population);
+
+        }                                                                             
+    }
+}
