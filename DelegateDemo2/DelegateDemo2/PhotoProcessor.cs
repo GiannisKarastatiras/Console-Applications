22fda04df5595c14b03b3cce4e629a43ed1a5bc6
GiannisKarastatiras <45010932+GiannisKarastatiras@users.noreply.github.com>
2019-01-03 21:36:15 +0200

    Add files via upload

---
+++ DelegateDemo2/DelegateDemo2/PhotoProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo2
+{
+    public class PhotoProcessor
+    {
+        //public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(string path, Action<Photo> /*PhotoFilterHandler*/ filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            //var filters = new PhotoFilters();
+            //filters.ApplyBrightness(photo);
+            //filters.ApplyContrast(photo);
+            //filters.Resize(photo);
+            filterHandler(photo);
+
+            photo.Save();
+        }
+    }
+}
---
+++ DelegateDemo2/DelegateDemo2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Flexibility + Extensibility---Delegate
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            /*PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;*/
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+
+            processor.Process("photo.jpg", filterHandler);
+        }
+
+        
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Aplly Remove Red Eye Filter");
+        }
+    }
+}
